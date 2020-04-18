e351a80dfef1dee80834f9e628a3d0f653dc5281
Eetu <datmixor@gmail.com>
2020-04-18 21:19:30 +0300

    made the program infinitely generate random numbers

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+	 "math/rand"
+         "time"
+)
+
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
 
 func main() {
+    rand.Seed(time.Now().UnixNano())
     var value = 0
     for true {
+	value = random(0, 99999)
     	fmt.Println(value)
-	value++
     }
 }
