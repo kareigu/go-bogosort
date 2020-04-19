297e24d97ba886335ad4287d24c393255ce5c49b
Eetu <datmixor@gmail.com>
2020-04-19 20:49:02 +0300

    Finished bogosort

--- src/main.go
+++ src/main.go
@@ -6,16 +6,46 @@ import (
 	 "time"
 )
 
+const arrayLength = 11
+var sortedArray = [arrayLength]int{1,2,3,4,5,6,7,8,9,10,11}
+
+func isEqual(a, b [arrayLength]int) bool {
+	if a == b {
+		return true
+	}
+	return false
+}
+
 func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func shuffleArray(input [arrayLength]int) [arrayLength]int {
+	rand.Shuffle(len(input), func(i, j int) { input[i], input[j] = input[j], input[i] })
+	return input
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	var i = 0
-	for i < 10 {
-		var value = random(0, 99999)
-		fmt.Println(value)
-		i++
+	var arrayToSort = sortedArray
+
+	var startTime = time.Now()
+
+	arrayToSort = shuffleArray(arrayToSort)
+	fmt.Printf("Original: %v\n", arrayToSort)
+
+	var isSorted bool = false
+	var iterations int = 0
+	for !isSorted {
+		arrayToSort = shuffleArray(arrayToSort)
+		if isEqual(sortedArray, arrayToSort) {
+			isSorted = true
+			var endTime = time.Now()
+			var elapsedTime = endTime.Sub(startTime)
+			fmt.Printf("Result: %v\n", arrayToSort)
+			fmt.Printf("Iterations: %v\n", iterations)
+			fmt.Printf("Time to complete: %v\n", elapsedTime)
+		}
+		iterations++
 	}
 }
